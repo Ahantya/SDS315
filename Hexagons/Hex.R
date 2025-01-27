9ab88d78e24412ee0c4649091da32f87577c5d3e
Ahantya <ahantyasharma57@gmail.com>
2025-01-27 16:56:08 -0600

    finished

---
+++ Hexagons/Hex.R
@@ -0,0 +1,41 @@
+# Install required packages
+install.packages("hexSticker")
+install.packages("ggplot2")
+library(hexSticker)
+library(ggplot2)
+
+img_url <- "stopwatch_dna.png"  
+
+sticker(
+  subplot = img_url,       
+  package = "",  
+  p_size = 2,
+  s_x = 1, s_y = 1,        
+  s_width = 1.2, s_height = 1.2,  
+  h_fill = NA,
+  h_color = "#1B4F72",
+  p_color = "white",  
+  dpi = 1200, 
+  white_around_sticker = TRUE,
+  filename = "survivalsurrogate_hex.png"
+)
+
+
+sticker(
+  subplot = img_url,       
+  package = "",  
+  p_size = 2,
+  s_x = 1, s_y = 1,        
+  s_width = 1.2, s_height = 1.2,  
+  h_fill = NA,
+  h_color = "#1B4F72",
+  p_color = "white",  
+  dpi = 1200, 
+  white_around_sticker = TRUE,
+  filename = "survivalsurrogate_hex.png"
+)
+
+
+
+
+
